20685051147ead1ee26f4419f53f5dcc7f2dc038
ramon-braga <ramonvbraga@gmail.com>
2023-10-18 11:15:42 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let char = new Knight('Knight');
+let monster = new BigMonster();
+let log = new Log(document.querySelector('.log'));
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start();
